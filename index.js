d63c9060a1332ae806e78f0217bd22404fc19b04
Harry Gallagher <harrygallagher4@gmail.com>
2019-02-17 22:59:59 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,212 @@
+import fs from 'fs';
+import path from 'path';
+import TorrentLibrary from 'torrent-files-library';
+import MovieDB from 'moviedb';
+import TVDB from 'node-tvdb';
+import moment from 'moment';
+import ptn from 'torrent-name-parser';
+import request from 'request';
+
+const CONFIG = require('./config.json');
+
+const { TMDB_API_KEY, TVDB_API_KEY, PUSHBULLET_API_KEY } = CONFIG.AUTH;
+const { PUSHBULLET } = CONFIG;
+const { MOVIE_PATH, TV_PATH, NEW_PATH } = CONFIG.PATHS;
+const { IGNORE_FILES } = CONFIG;
+
+const tmdb = MovieDB(TMDB_API_KEY);
+const tvdb = new TVDB(TVDB_API_KEY);
+const tl = new TorrentLibrary();
+
+
+tl.addNewPath(NEW_PATH)
+  .then(message => {
+    scanAndMove();
+    watch();
+  })
+  .catch(error => {
+    console.log(error);
+  });
+
+function watch() {
+  fs.watch(NEW_PATH, (type, file) => {
+    console.log(`\tDetected change ${type} ${file}, scanning...`);
+    scanAndMove();
+  });
+}
+
+function fileInfo(file) {
+  const torrent = ptn(path.basename(file));
+  const dir = path.dirname(file);
+  const ext = path.extname(file);
+  const parentDir = path.dirname(dir);
+
+  return { torrent, dir, ext, parentDir };
+}
+
+function tmdbDestination(match, extension) {
+  const year = tmdbMatchYear(match);
+  const name = movieName({ title: match.title, year: year });
+  const folder = movieFolder(name);
+  const path = moviePath(folder, name, extension);
+
+  return { year, name, folder, path };
+}
+
+function tmdbMatchYear(match) {
+  return moment(match.release_date).toObject().years;
+}
+
+function movieName({ title, year }) {
+  return `${title} (${year})`;
+}
+
+function movieFolder(name) {
+  return `${MOVIE_PATH}/${name}`;
+}
+
+function moviePath(folder, name, extension) {
+  return `${folder}/${name}${extension}`;
+}
+
+function processMovie(file) {
+
+  const info = fileInfo(file);
+  const { torrent, dir, ext, parentDir } = info;
+
+  if (IGNORE_FILES.includes(torrent.title.toLowerCase())) {
+    console.log(`\tIgnoring file: ${file}`);
+    console.log(`\tIf this is an error, remove '${torrent.title.toLowerCase()}' from IGNORE_FILES`);
+    return;
+  }
+
+  tmdb.searchMovie({
+    query: torrent.title,
+    year: torrent.year
+  }, (e, r) => {
+    if (e || !r || !r.results || !r.results[0]) {
+      console.log(`NO MATCH! ${torrent.title}`);
+      return;
+    }
+
+    const match = r.results[0];
+
+    const destination = tmdbDestination(match, ext);
+    const { year, name, folder, path } = destination;
+
+    try {
+      if (!fs.existsSync(folder)) {
+        fs.mkdirSync(folder);
+      }
+
+      console.log(`MOVING:\t${file} => ${path}`);
+      fs.rename(file, path, e => { console.log(e); });
+
+      const options = {
+        headers: {
+          'Access-Token': PUSHBULLET_API_KEY
+        },
+        uri: 'https://api.pushbullet.com/v2/pushes',
+        method: 'POST',
+        json: {
+          'channel_tag': `${PUSHBULLET.CHANNEL_TAG}`,
+          'type': 'note',
+          'title': `${PUSHBULLET.SERVER_NAME} - ${name}`,
+          'body': `${name} was added to ${PUSHBULLET.SERVER_NAME}`
+        }
+      };
+
+      request(options, (e, r, b) => {
+        console.log(e, b);
+      });
+
+    } catch (e) {
+      console.error(e);
+    }
+  });
+}
+
+function tvSeriesPath(torrent) {
+  return `${TV_PATH}/${torrent.title}`;
+}
+
+function tvSeasonPath(seriesPath, torrent) {
+  return `${seriesPath}/Season ${torrent.season}`;
+}
+
+function tvEpisodePath(seasonPath, torrent, ext) {
+  return `${seasonPath}/${torrent.title} S${torrent.season}E${torrent.episode}${ext}`;
+}
+
+function tvShowDestination(torrent, ext) {
+  const seriesPath = tvSeriesPath(torrent);
+  const seasonPath = tvSeasonPath(seriesPath, torrent);
+  const episodePath = tvEpisodePath(seasonPath, torrent, ext);
+
+  return { seriesPath, seasonPath, episodePath };
+}
+
+function processTV(file) {
+
+  const info = fileInfo(file);
+  const { torrent, dir, ext, parentDir } = info;
+
+  if (IGNORE_FILES.includes(torrent.title.toLowerCase())) {
+    console.log(`\tIgnoring file: ${file}`);
+    console.log(`\tIf this is an error, remove '${torrent.title.toLowerCase()}' from IGNORE_FILES`);
+    return;
+  }
+
+  const destination = tvShowDestination(torrent, ext);
+  const { seriesPath, seasonPath, episodePath } = destination;
+
+  try {
+    if (!fs.existsSync(seriesPath)) {
+      console.log(`Series path ${seriesPath} does not exist, creating...`);
+      fs.mkdirSync(seriesPath);
+    }
+
+    if (!fs.existsSync(seasonPath)) {
+      console.log(`Season path ${seasonPath} does not exist, creating...`);
+      fs.mkdirSync(seasonPath);
+    }
+
+    console.log(`MOVING:\t${file} => ${episodePath}`);
+    fs.rename(file, episodePath, e => { console.log(e); });
+  } catch (e) {
+    console.error(e);
+  }
+
+}
+
+function scanAndMove() {
+  tl.scan()
+    .then(message => {
+      tl.allFilesWithCategory.forEach((type, file) => {
+        if (type == 'MOVIES') {
+          if (!fs.existsSync(file)) {
+            console.log(`[MOVIES] Skipping: ${file}`);
+            return;
+          }
+
+          //TODO this is a hack
+          if (file.toLowerCase().includes('sample')) {
+            console.log(`[MOVIES] Detected sample file, skipping: ${file}`);
+            return;
+          }
+
+          console.log(`[MOVIES] Processing: ${file}`);
+          processMovie(file);
+        } else if (type == 'TV_SERIES') {
+          console.log(`[TV] Processing: ${file}`);
+          processTV(file);
+        } else {
+          console.log(`[${type}] Ignoring: ${file}`);
+        }
+      });
+    })
+    .catch(error => {
+      console.log('ERROR:');
+      console.log(error);
+    });
+}
